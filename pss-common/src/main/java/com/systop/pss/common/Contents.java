8e4750fddcdf38049b4699c764ca6c47d871ca12
Administrator <Administrator@OXN2I59Z0CRBBGD>
2019-11-25 22:09:48 +0800

    up

--- pss-common/src/main/java/com/systop/pss/common/Contents.java
+++ pss-common/src/main/java/com/systop/pss/common/Contents.java
@@ -2,5 +2,13 @@ package com.systop.pss.common;
 
 public class Contents {
 
+	/**
+	 *  钢排钉编号
+	 */
 	private static final Integer PRODUCT_STEEL_NAILS = 1;
+	
+	/**
+	 * 卷钉编号
+	 */
+	private static final Integer PRODUCT_ROLL_NAILS = 2;
 }
